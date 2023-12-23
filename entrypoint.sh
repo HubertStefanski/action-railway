51d0a4d41f6303cbd96260e3414fb413e8a0b2ce
hstefans <hstefans@redhat.com>
2023-12-23 21:49:37 +0100

    add railway_service to railway deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -l
 
-if [ $1 ]; then
+if [ $1 && $2 ]; then
     export RAILWAY_TOKEN=$1
+    export RAILWAY_SERVICE=$2
+else
+  echo "RAILWAY_TOKEN and RAILWAY_SERVICE must be defined"
+
 fi
 
-railway up --detach || error_code=$?
+railway up --service ${RAILWAY_SERVICE} --detach || error_code=$?
 
 if [ "${error_code}" -ne 0 ]; then
     exit $error_code
